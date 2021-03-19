8e5dea1f86be2c324311cf4e0ced0092ef3e50cc
skinnypete65 <skinnypetetskipe@gmail.com>
2021-03-20 01:17:57 +0300

    Добавил вычисление выражения в префиксной нотации

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -34,9 +34,57 @@ public:
 	void clear();
 };
 
-// Main programm
+template<typename T>
+class Deque {
+public:
+	Deque();
+	~Deque();
+
+	void push_back(T data);
+	void push_front(T data);
+	T pop_front();
+	T pop_back();
+	T getFirst() { return head->data; }
+	T getLast() { return tail->data; }
+	int getSize() { return Size; }
+	void clear();
+	T& operator[](const int index);
+	bool isEmpty();
+
+private:
+
+	template<typename T>
+	class Node {
+	public:
+		Node* pNext;
+		Node* pPrev;
+		T data;
+
+		Node(T data = T(), Node* pNext = nullptr, Node* pPrev = nullptr) {
+			this->data = data;
+			this->pNext = pNext;
+			this->pPrev = pPrev;
+		}
+
+	};
+
+	Node<T>* head;
+	Node<T>* tail;
+	int Size;
+};
+
+/***********************
+	Helper functions
+************************/
 bool isOperator(char ch) {
 	if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+		return true;
+	}
+	else return false;
+}
+
+bool isOperator(string ch) {
+	if (ch == "+" || ch == "-" || ch == "*" || ch == "/") {
 		return true;
 	}
 	else return false;
@@ -184,7 +232,7 @@ string fromInfixToPrefix(string s) {
 	return prefix;
 }
 
-void makeOperation(Stack<int>& operandStack, Stack<char>& operStack) {
+void makeOperationToStack(Stack<int>& operandStack, Stack<char>& operStack) {
 	int operand2 = operandStack.pop();
 	int operand1 = operandStack.pop();
 	char operation = operStack.pop();
@@ -195,10 +243,28 @@ void makeOperation(Stack<int>& operandStack, Stack<char>& operStack) {
 	else if (operation == '/') operandStack.push(operand1 / operand2);
 }
 
+int makeOperation(string operand1, string oper, string operand2) {
+	int intOperand1 = stoi(operand1);
+	int intOperand2 = stoi(operand2);
+	if (oper == "+") {
+		return intOperand1 + intOperand2;
+	}
+	else if (oper == "-") {
+		return intOperand1 - intOperand2;
+	}
+	else if (oper == "*") {
+		return intOperand1 * intOperand2;
+	}
+	else if (oper == "/") {
+		return intOperand1 / intOperand2;
+	}
+
+
+}
+
 int calculateInfix(string s) {
 	string infix = s;
 	string strNum = "";
-	int num;
 	int Error = 0; // if Error = 0, everything if OK
 	Stack<int> operandStack; // Stack with numbers
 	Stack<char> operStack; // Stack with operations
@@ -210,7 +276,7 @@ int calculateInfix(string s) {
 				strNum = "";
 			}
 			while (operStack.getTop() != '(') {
-				makeOperation(operandStack, operStack);
+				makeOperationToStack(operandStack, operStack);
 			}
 			operStack.pop();
 		}
@@ -229,7 +295,7 @@ int calculateInfix(string s) {
 				operStack.push(ch);
 			}
 			else {
-				makeOperation(operandStack, operStack);
+				makeOperationToStack(operandStack, operStack);
 
 				operStack.push(ch);
 			}
@@ -239,7 +305,7 @@ int calculateInfix(string s) {
 	if (strNum != "") operandStack.push(stoi(strNum));
 	
 	while (!(operStack.isEmpty())) {
-		makeOperation(operandStack, operStack);
+		makeOperationToStack(operandStack, operStack);
 	}
 
 	return operandStack.pop();
@@ -262,7 +328,7 @@ int calculatePostfix(string s) {
 				strNum = "";
 			}
 			operStack.push(ch);
-			makeOperation(operandStack, operStack);
+			makeOperationToStack(operandStack, operStack);
 		}
 		else {
 			strNum += ch;
@@ -272,6 +338,73 @@ int calculatePostfix(string s) {
 	return operandStack.pop();
 }
 
+int calculatePrefix(string s) {
+	string prefix = s;
+	string strNum = "";
+	Deque<string> deque;
+	
+	// Parse string to deque
+	for (char ch : prefix) {
+		if (ch == ' ') {
+			if (strNum != "") {
+				deque.push_back(strNum);
+				strNum = "";
+			}
+		}
+		else if (isOperator(ch)) {
+			if (strNum != "") {
+				deque.push_back(strNum);
+				strNum = "";
+			}
+			string operation;
+			operation += ch;
+			deque.push_back(operation);
+			
+		}
+		else {
+			strNum += ch;
+		}
+	}
+	if (strNum != "") {
+		deque.push_back(strNum);
+		strNum = "";
+	}
+
+	string val1, val2, val3, result = "";
+	while (deque.getSize() != 1) {
+		val1 = deque.pop_front();
+		if (!(isOperator(val1))) {
+			deque.push_back(val1);
+			continue;
+		}
+
+		val2 = deque.getFirst();
+		if (isOperator(val2)) {
+			deque.push_back(val1);
+			continue;
+		}
+		val2 = deque.pop_front();
+
+		val3 = deque.getFirst();
+		if (isOperator(val3)) {
+			deque.push_back(val1);
+			deque.push_front(val2);
+			continue;
+		}
+
+		val3 = deque.pop_front();
+
+		string result = to_string(makeOperation(val2, val1, val3));
+		deque.push_back(result);
+	}
+
+	return stoi(deque.pop_front());
+
+}
+
+/***********************
+	Main Programm
+************************/
 int main() {
 	setlocale(LC_ALL, "Russian");
 
@@ -290,6 +423,8 @@ int main() {
 	int result_2a = calculateInfix(infix_2a);
 	int result_2b = calculateInfix(infix_2b);
 
+	string prefix_3 = "-+12*2 3*/16 8 4";
+
 	string postfix_4 = "12 2 3 *16 8 -*+";
 
 	cout << "\tTask 1:" << endl;
@@ -306,16 +441,23 @@ int main() {
 	cout << "Result: " << result_2a << endl;
 	cout << "b) Infix form: " << infix_2b << endl;
 	cout << "Result: " << result_2b << endl;
+	cout << endl; 
+
+	cout << "\tTask 3:" << endl;
+	cout << "Prefix from: " << prefix_3 << endl;
+	cout << "Result: " << calculatePrefix(prefix_3) << endl;
 	cout << endl;
 
 	cout << "\tTask4:" << endl;
 	cout << "Postfix form: " << postfix_4 << endl;
-	cout << calculatePostfix(postfix_4) << endl;
+	cout << "Result: " << calculatePostfix(postfix_4) << endl;
 	cout << endl;
 }
 
 
-// Methods of class Stack
+/***************************
+	Methods of class Stack
+***************************/
 template<typename T>
 inline Stack<T>::Stack()
 {
@@ -380,4 +522,117 @@ void Stack<T>::clear()
 	while (Size) {
 		pop();
 	}
+}
+
+/**************************
+	Methods of class Deque
+***************************/
+template<typename T>
+Deque<T>::Deque() {
+	this->Size = 0;
+	head = nullptr;
+	tail = nullptr;
+}
+
+template<typename T>
+Deque<T>::~Deque() {
+	clear();
+}
+
+template<typename T>
+void Deque<T>::push_back(T data) {
+	if (head == nullptr) {
+		head = new Node<T>(data);
+		tail = head;
+	}
+	else {
+		Node<T>* newNode = new Node<T>(data);
+		newNode->pNext = nullptr;
+		newNode->pPrev = tail;
+		tail->pNext = newNode;
+		tail = newNode;
+	}
+
+	Size++;
+}
+
+template<typename T>
+void Deque<T>::push_front(T data) {
+	if (head == nullptr) {
+		head = new Node<T>(data);
+		tail = head;
+	}
+	else {
+		Node<T>* newNode = new Node<T>(data, head, nullptr);
+		head->pPrev = newNode;
+		head = newNode;
+	}
+	Size++;
+}
+
+template<typename T>
+T Deque<T>::pop_front() {
+	T tempData = head->data;
+	if (Size == 1) {
+		//head->data = NULL;
+		head = nullptr;
+		tail = head;
+	}
+	else {
+		Node<T>* temp = head;
+		head = head->pNext;
+		head->pPrev = nullptr;
+		delete temp;
+	}
+
+	Size--;
+	return tempData;
+}
+
+template<typename T>
+T Deque<T>::pop_back() {
+	T tempData = tail->data;
+	if (Size == 1) {
+		tail->data = NULL;
+		tail = nullptr;
+		head = tail;
+	}
+	else {
+		Node<T>* temp = tail;
+		tail = tail->pPrev;
+		tail->pNext = nullptr;
+		delete temp;
+	}
+
+	Size--;
+	return tempData;
+}
+
+template<typename T>
+void Deque<T>::clear()
+{
+	while (Size) {
+		pop_front();
+	}
+}
+
+template<typename T>
+T& Deque<T>::operator[](const int index)
+{
+	int counter = 0;
+	Node<T>* current = this->head;
+
+	while (current != nullptr) {
+		if (counter == index) {
+			return current->data;
+		}
+		current = current->pNext;
+		counter++;
+	}
+}
+
+template<typename T>
+bool Deque<T>::isEmpty() {
+	if (head == nullptr) return true;
+	else return false;
 }
